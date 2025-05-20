e6e2bb70b85cd114d4db2c779fa51802b3477142
Prachi <prachipratyushaacharya@gmail.com>
2025-05-20 22:03:59 +0530

    up.7

--- app.js
+++ app.js
@@ -7,9 +7,34 @@ const addTask = () => {
 
     if(text){
         tasks.push({tex: text, completed: false });
-        
+        taskInput.value = "";
+        updateTasksList();
     }
-}
+};
+
+const updateTasksList = () => {
+    const taskList = document.getElementById('task-list');
+    taskList.innerHTML = ''
+
+    tasks.forEach((task,index) =>{
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+        <div class = "taskItem">
+            <div class="task $ {task.completed ? "completed": ""}">
+                <input type= "checkbox" class="checkbox" $ {task.completed ? "checked" : ""} />
+                <p> $ {task.text} </p>
+            </div>
+            <div class= "icons">
+                <img src= "./TO-DO APP/edit-icon.png" onClick= "editTask(${index})" />
+                <img src= "./TO-DO APP/bin-icon.png" onClick= "deleteTask(${index})"/>
+            </div>
+        </div>
+        `;
+        listItem.addEventListener("change", ()=> toggleTastComplete(index));
+        taskList.append(ListItem);
+    });
+};
 
 document.getElementById('newTask').addEventListener('click', function(e)
 {
